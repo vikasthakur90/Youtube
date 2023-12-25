1f13f5536790270a8bec9eb6800ea0deba881902
Vikas Thakur <66615520+vikasthakur90@users.noreply.github.com>
2023-12-25 22:24:56 +0530

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,3 @@
-const GOOGLE_API_KEY = "AIzaSyAvuJ6K59uM8bJxS6K2n0cDuxjULQlO4nA";
+const GOOGLE_API_KEY = "AIzaSyA9uM8bJxSxjULQlO4nA";
 export const YOUTUBE_VIDEO_URL = "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key="+GOOGLE_API_KEY;
 export const YOUTUBE_SEARCH_API="http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
